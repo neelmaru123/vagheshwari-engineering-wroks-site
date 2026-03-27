264ea7f49ea47e956472ac9e8d292f985d52d018
Neel Maru <121446887+neelmaru123@users.noreply.github.com>
2026-03-27 16:06:20 +0530

    anaytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title:
@@ -90,6 +91,7 @@ export default function RootLayout({
             }),
           }}
         />
+        <Analytics />
         <Providers>{children}</Providers>
       </body>
     </html>
